854e21a549d3a4c57213a383ac566d805c32193b
albert.szymanski <albert.szymanski@cetuspro.com>
2025-01-12 11:58:20 +0100

    feat: added refreshing user access token for spotify

--- SpotifAi/SpotifAi/Spotify/Api/AttachUserAccessTokenDelegatingHandler.cs
+++ SpotifAi/SpotifAi/Spotify/Api/AttachUserAccessTokenDelegatingHandler.cs
@@ -2,12 +2,16 @@
 using Microsoft.EntityFrameworkCore;
 using SpotifAi.Auth.RequestContext;
 using SpotifAi.Persistence;
+using SpotifAi.Spotify.Api.Authorization;
+using SpotifAi.Utils;
 
 namespace SpotifAi.Spotify.Api;
 
 internal sealed class AttachUserAccessTokenDelegatingHandler(
     AppDbContext dbContext,
-    IRequestContext requestContext
+    IRequestContext requestContext,
+    IClock clock,
+    ISpotifyAuthorizationApi spotifyAuthorizationApi
 ) : DelegatingHandler
 {
     protected override async Task<HttpResponseMessage> SendAsync(
@@ -17,8 +21,21 @@ internal sealed class AttachUserAccessTokenDelegatingHandler(
         var spotifyToken = await dbContext.SpotifyAccessTokens
             .FirstOrDefaultAsync(x => x.UserId == requestContext.Id, cancellationToken);
 
-        if (spotifyToken is not null)
+        if (spotifyToken is null)
+            throw new InvalidOperationException("User has not authorized Spotify");
+
+        if (spotifyToken.ExpiresAt < clock.Now)
             request.Headers.Authorization = new AuthenticationHeaderValue("Bearer", spotifyToken.AccessToken);
+
+        var refreshToken = await spotifyAuthorizationApi.RefreshAccessTokenAsync(spotifyToken.RefreshToken);
+
+        spotifyToken.AccessToken = refreshToken.AccessToken;
+        spotifyToken.ExpiresAt = clock.Now.AddSeconds(refreshToken.ExpiresIn);
+        spotifyToken.RefreshToken = refreshToken.RefreshToken;
+
+        await dbContext.SaveChangesAsync(cancellationToken);
+
+        request.Headers.Authorization = new AuthenticationHeaderValue("Bearer", refreshToken.AccessToken);
 
         return await base.SendAsync(request, cancellationToken);
     }
